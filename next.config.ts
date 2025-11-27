0871922aeb119aab0e5c1aa84ea2fdcaeeb84d03
KhangQuach0405 <quachthieukhang@gmail.com>
2025-11-27 16:55:23 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/auth/login', // <-- Đổi thành route bạn muốn làm mặc định
-        permanent: false, // `false` để trình duyệt không cache redirect này vĩnh viễn
-      },
-    ]
-  },
+  
   images: {
     
     domains: ['localhost'],
